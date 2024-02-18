998f30ca5f65e02f6a66d8bd5fbcdd4558ff5d4f
ARU0007 <aruneshagrahari95@gmail.com>
2024-02-18 15:00:09 +0530

    create Main.java for method calling.

---
+++ src/main/Main.java
@@ -0,0 +1,16 @@
+package main;
+
+import singly.linkedlist.SinglyLinkedList;
+
+public class Main {
+    public static void main(String[] args) {
+        SinglyLinkedList<Integer> ll = new SinglyLinkedList<>();
+
+        //calling methods''''''''''''''''''''
+
+        ll.create();
+        ll.traverse();
+    }
+
+
+}
